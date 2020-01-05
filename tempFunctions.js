68ee585438b6da1e55b0ea66e07dfe026a5e89af
Ian Turner <ianturner88@gmail.com>
2020-01-04 23:39:50 -0500

    Issues with click

---
+++ tempFunctions.js
@@ -0,0 +1,12 @@
+// Listen for submit
+document.getElementById("Colin").addEventListener('submit', calculateResults);
+
+// Calculate results
+function calculateResults(e){
+    console.log('Calculating...');
+    
+    // UI Vars
+
+
+    e.preventDefault();
+}
